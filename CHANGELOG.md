4a0763412c949b31e1b88adac5de7bc343a824e5
Jee <zeddnyx08@gmail.com>
2025-05-08 08:17:10 +0700

    feat: create other variant of carousel

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -2,6 +2,12 @@
 
 All notable changes to this project will be documented in this file.
 
+## [0.1.0] - 2025-05-08
+
+### Added
+
+- (Feat) Other variant of carousel
+
 ## [0.1.0] - 2025-04-27
 
 ### Addedd
--- components/module/example/demo-carousel.tsx
+++ components/module/example/demo-carousel.tsx
@@ -6,24 +6,49 @@ import { useState } from "react";
 
 export default function DemoCarousel() {
   const [current, setCurrent] = useState(0);
+  const [current2, setCurrent2] = useState(0);
   return (
-    <CCarousel
-      arrowPosition="inside-right"
-      showDots
-      current={current}
-      setCurrent={setCurrent}
-    >
-      {Array.from({ length: 5 }).map((_, index) => (
-        <CarouselItem key={index} className="md:basis-1/2 lg:basis-1/3 h-20">
-          <div
-            className={cn("p-5 bg-red-600", {
-              "bg-neutral-400!": index == current,
-            })}
+    <>
+      <CCarousel
+        arrowPosition="inside-right"
+        showDots
+        current={current}
+        setCurrent={setCurrent}
+      >
+        {Array.from({ length: 5 }).map((_, index) => (
+          <CarouselItem key={index} className="md:basis-1/2 lg:basis-1/3 h-20">
+            <div
+              className={cn("p-5 bg-red-600", {
+                "bg-neutral-400!": index == current,
+              })}
+            >
+              <span className="text-3xl font-semibold">{index + 1}</span>
+            </div>
+          </CarouselItem>
+        ))}
+      </CCarousel>
+      <CCarousel
+        showArrow={false}
+        showDots
+        current={current2}
+        setCurrent={setCurrent2}
+      >
+        {Array.from({ length: 5 }).map((_, index) => (
+          <CarouselItem
+            key={index}
+            className="md:basis-1/2 lg:basis-1/3 h-40 flex items-center w-full"
           >
-            <span className="text-3xl font-semibold">{index + 1}</span>
-          </div>
-        </CarouselItem>
-      ))}
-    </CCarousel>
+            <div
+              className={cn("p-5 bg-red-600 size-full", {
+                "bg-neutral-400!": current2 == index,
+                "opacity-50 h-20": current2 != index,
+              })}
+            >
+              <span className="text-3xl font-semibold">{index + 1}</span>
+            </div>
+          </CarouselItem>
+        ))}
+      </CCarousel>
+    </>
   );
 }
